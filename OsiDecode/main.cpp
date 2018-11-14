f9609487825e114b035eb0af527c1206e5161e24
TalaourarAli <33072626+TalaourarAli@users.noreply.github.com>
2018-11-14 16:32:55 +0100

    Testes!

--- OsiDecode/main.cpp
+++ OsiDecode/main.cpp
@@ -7,12 +7,11 @@ using namespace std;
 
 int main()
 {
-    string S="~~~";
+    string S="Hello!!";
     Coding c(S);
     string coded=c.coder();
     cout << "original string :"<<S<<endl;
     cout<<"coded string (to send) :"<<coded<<endl;
-
     Decoding d(coded);
     cout <<"encoded string received :"<<d.decode()<<endl<<endl;
     return 0;
